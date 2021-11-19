232ef9f35850470acf73aa91fac6705da713ff1e
Raudo Moquete <raudo.moquete@rhodium.com.do>
2021-11-19 09:48:16 -0500

    Users roles and role admin

--- CmsShoppingCart/Areas/Admin/Controllers/RolesController.cs
+++ CmsShoppingCart/Areas/Admin/Controllers/RolesController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -21,6 +22,77 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
             _userManager = userManager;
         }
 
+        //GET /admin/roles
         public IActionResult Index() => View(_roleManager.Roles);
+
+        //GET /admin/roles/create
+        public IActionResult Create() => View();
+
+        //POST /admin/roles/create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task <IActionResult> Create([MinLength(2), Required] string name)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityResult result = await _roleManager.CreateAsync(new IdentityRole(name));
+                if (result.Succeeded)
+                {
+                    TempData["Success"] = "The role has been created!";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (IdentityError error in result.Errors) ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            ModelState.AddModelError("", "Minimum length is 2");
+            return View();
+        }
+
+        //GET /admin/roles/edit/id
+        public async Task<IActionResult> Edit(string id)
+        {
+            IdentityRole role = await _roleManager.FindByIdAsync(id);
+
+            List<AppUser> members = new List<AppUser>();
+            List<AppUser> nonmembers = new List<AppUser>();
+
+            foreach (AppUser user in _userManager.Users)
+            {
+                var list = await _userManager.IsInRoleAsync(user, role.Name) ? members : nonmembers;
+                list.Add(user);
+            }
+
+            return View(new RoleEdit 
+            { 
+                Role = role,
+                Members = members,
+                NonMembers = nonmembers
+            });
+        }
+
+        //POST /admin/roles/edit/id
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(RoleEdit roleEdit)
+        {
+            IdentityResult result;
+                                                         //Si el arreglo esta vacio no pasara nada
+            foreach (string userId in roleEdit.AddIds ?? new string[] { })
+            {
+                AppUser user = await _userManager.FindByIdAsync(userId);
+                result = await _userManager.AddToRoleAsync(user, roleEdit.RoleName);
+            }
+                                                            //Si el arreglo esta vacio no pasara nada
+            foreach (string userId in roleEdit.DeleteIds ?? new string[] { })
+            {
+                AppUser user = await _userManager.FindByIdAsync(userId);
+                result = await _userManager.RemoveFromRoleAsync(user, roleEdit.RoleName);
+            }
+
+            return Redirect(Request.Headers["Referer"].ToString());
+        }
     }
 }
